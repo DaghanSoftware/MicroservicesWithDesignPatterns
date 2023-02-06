56101044dda7890cbb5e44d9ec2d99e0e050331e
Semih DAGHAN <daghansoftware@hotmail.com>
2023-02-06 21:13:39 +0300

    Order.API  Modeller Olusturuldu  ve EfCore  Paketleri Projeye dahil edildi. Microsoft.EntityFrameworkCore Microsoft.EntityFrameworkCore.Design Microsoft.EntityFrameworkCore.Tools Microsoft.EntityFrameworkCore.SqlServer AppDbContext' Nesnesi oluşturuldu ve Migration ile database tabloları tamamlandı.

--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Order.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("LocalDb"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
