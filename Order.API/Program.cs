fc209229a5f94b7a9484876c40765ed61fe04d96
Semih DAGHAN <daghansoftware@hotmail.com>
2023-02-09 19:05:34 +0300

    RabbitMQ Bağlantısı gerçekleştirildi.

--- Order.API/Program.cs
+++ Order.API/Program.cs
@@ -1,13 +1,25 @@
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Order.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMassTransit(x => {
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration.GetConnectionString("RabbitMQ"));
+    });
+});
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
     x.UseSqlServer(builder.Configuration.GetConnectionString("LocalDb"));
 });
+builder.Services.AddOptions<MassTransitHostOptions>()
+    .Configure(options =>
+    {
+    });
+//builder.Services.AddMassTransitHostedService();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,7 +33,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
