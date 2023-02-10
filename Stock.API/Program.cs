0a673e82d7fe62f10f627271ec958fa65a14d327
Semih DAGHAN <daghansoftware@hotmail.com>
2023-02-10 15:37:13 +0300

    Stock API (Microservice) OrderCreatedEvent için consumer oluşturma

--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,13 +1,32 @@
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
+using Shared;
+using Stock.API.Consumers;
 using Stock.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMassTransit(x => {
+
+    x.AddConsumer<OrderCreatedEventConsumer>();
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration.GetConnectionString("RabbitMQ"));
+        cfg.ReceiveEndpoint(RabbitMQSettingsConst.StockOrderCreatedEventQueueName, e =>
+        {
+            e.ConfigureConsumer<OrderCreatedEventConsumer>(context);
+        });
+    });
+
+});
+
 builder.Services.AddDbContext<AppDbContext>(x =>
 {
     x.UseInMemoryDatabase("StockDb");
 });
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,7 +40,20 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+builder.Services.AddOptions<MassTransitHostOptions>()
+    .Configure(options =>
+    {
+        // if specified, waits until the bus is started before
+        // returning from IHostedService.StartAsync
+        // default is false
+        options.WaitUntilStarted = true;
 
+        // if specified, limits the wait time when starting the bus
+        options.StartTimeout = TimeSpan.FromSeconds(10);
+
+        // if specified, limits the wait time when stopping the bus
+        options.StopTimeout = TimeSpan.FromSeconds(30);
+    });
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
